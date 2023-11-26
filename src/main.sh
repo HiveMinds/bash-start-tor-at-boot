e56238569b1ca6a8ba4aefe69867b684fd67381d
a-t-0 <you@example.com>
2023-11-26 02:30:51 +0100

    Changed absolute source loading to relative.

--- src/main.sh
+++ src/main.sh
@@ -6,9 +6,18 @@ source dependencies/bash-log/src/main.sh
 LOG_LEVEL_ALL # set log level to all, otherwise, NOTICE, INFO, DEBUG, TRACE will not be logged.
 
 # Load prerequisites installation.
-source src/activate_systemct_services.sh
-source src/uwf_status.sh
-source src/installation.sh
+function load_functions() {
+  local script_dir
+  script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+  # shellcheck disable=SC1091
+  source "$script_dir/activate_systemct_services.sh"
+  # shellcheck disable=SC1091
+  source "$script_dir/src/uwf_status.sh"
+  # shellcheck disable=SC1091
+  source "$script_dir/installation.sh"
+}
+load_functions
 
 # Execute prerequisites installation.
 install_ssh_requirements
