af0a12989e1b58ece929813c369664446f8e4d68
a-t-0 <you@example.com>
2023-12-01 23:06:59 +0100

    Separated SSH setup at boot from tor setup at boot.

--- src/configuration.sh
+++ src/configuration.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-function configure_tor_to_start_at_boot() {
+function ensure_ssh_is_started_at_boot() {
   ensure_service_is_started "ssh"
-  ensure_service_is_started "tor"
   ensure_service_starts_at_boot "ssh"
 
   ensure_uwf_is_enabled
@@ -12,6 +11,12 @@ function configure_tor_to_start_at_boot() {
   # Source: Comment in: https://askubuntu.com/q/1264335
   # NO SUDO REQUIRED.
   chmod g-w ~/.ssh/config >>/dev/null 2>&1
+}
+
+function configure_tor_to_start_at_boot() {
+  ensure_ssh_is_started_at_boot
+
+  ensure_service_is_started "tor"
 
   # Ensure tor is started in the background.
   start_tor_in_background
