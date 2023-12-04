9ac34c9cf62563b5d0f7734eb7dfb7c364a9b489
a-t-0 <you@example.com>
2023-12-04 19:24:03 +0100

    Included apt pip installation requirement.

--- src/installation.sh
+++ src/installation.sh
@@ -8,5 +8,6 @@ function install_tor_and_ssh_requirements() {
   ensure_apt_pkg "openssh-client" 0
   ensure_apt_pkg "torsocks" 0
   ensure_apt_pkg "tor" 1
+  ensure_apt_pkg "pip" 1
   ensure_pip_pkg "dash" 0
 }
