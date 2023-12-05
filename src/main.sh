c9311639a3ed9abf1a92081169c55756a3fe97a3
a-t-0 <you@example.com>
2023-12-05 20:35:04 +0100

    Reduced boilerplate.

--- src/main.sh
+++ src/main.sh
@@ -1,75 +1,39 @@
 #!/bin/bash
 
-# This module is a dependency for:
-# - bash-create-onion-domains
-# - bash-ssh-over-tor
+# This module is a dependency for (and has these dependencies):
+START_TOR_AT_BOOT_PARENT_DEPS=("bash-start-tor-at-boot" "bash-ssh-over-tor")
 # This module has dependencies:
-# - bash-log
-# - bash-package-installer
+START_TOR_AT_BOOT_REQUIRED_DEPS=("bash-log" "bash-package-installer")
 
 START_TOR_AT_BOOT_SRC_PATH=$(dirname "$(readlink -f "$0")")
 START_TOR_AT_BOOT_PATH=$(readlink -f "$START_TOR_AT_BOOT_SRC_PATH/../")
 
-function load_dependencies() {
-  local dependency_or_parent_path
-  # The path of this repo ends in /bash-create-onion-domains. If it follows:
-  # /dependencies/bash-create-onion-domains, then it is a dependency of
-  #another module.
-  echo "START_TOR_AT_BOOT_PATH=$START_TOR_AT_BOOT_PATH"
-  if [[ "$START_TOR_AT_BOOT_PATH" == *"/dependencies/bash-create-onion-domains" ]]; then
-    # This module is a dependency of another module.
-    dependency_or_parent_path=".."
+# Loads the bash log dependency, and the dependency loader.
+function load_bash_log_dependency() {
+  if [ -d "$START_TOR_AT_BOOT_PATH/dependencies/bash-log" ]; then
+    # shellcheck disable=SC1091
+    source "$START_TOR_AT_BOOT_PATH/dependencies/bash-log/src/main.sh"
+  elif [ -d "$START_TOR_AT_BOOT_PATH/../bash-log" ]; then
+    # shellcheck disable=SC1091
+    source "$START_TOR_AT_BOOT_PATH/../bash-log/src/main.sh"
   else
-    dependency_or_parent_path="dependencies"
-  fi
-  echo "dependency_or_parent_path=$dependency_or_parent_path"
-  load_required_dependencies "$dependency_or_parent_path"
-  load_parent_dependencies "$dependency_or_parent_path"
-}
-
-function load_required_dependency() {
-  local dependency_or_parent_path="$1"
-  local dependency_name="$2"
-  local dependency_dir="$START_TOR_AT_BOOT_PATH/$dependency_or_parent_path/$dependency_name"
-  echo "dependency_dir=$dependency_dir"
-  if [ ! -d "$dependency_dir" ]; then
-    echo "ERROR: $dependency_dir is not found in required dependencies."
+    echo "ERROR: bash-log dependency is not found."
     exit 1
   fi
-  source "$dependency_dir/src/main.sh"
 }
+load_bash_log_dependency
 
-function load_required_dependencies() {
-  local dependency_or_parent_path="$1"
-  local required_dependencies=("bash-log" "bash-package-installer")
-  # Iterate through dependencies and check if they exist and load them.
-  for required_dependency in "${required_dependencies[@]}"; do
-    load_required_dependency "$dependency_or_parent_path" "$required_dependency"
-  done
-}
+# Load required dependencies.
+for required_dependency in "${START_TOR_AT_BOOT_REQUIRED_DEPS[@]}"; do
+  load_required_dependency "$START_TOR_AT_BOOT_PATH" "$required_dependency"
+done
 
-function load_parent_dependencies() {
-  local dependency_or_parent_path="$1"
-  local parent_dependencies=("bash-start-tor-at-boot" "bash-ssh-over-tor")
-  # Iterate through dependencies and check if they exist and load them.
-  for parent_dep in "${parent_dependencies[@]}"; do
-    local parent_dep_dir="$START_TOR_AT_BOOT_PATH/../$parent_dep"
-    echo "parent_dep_dir=$parent_dep_dir"
-    # Check if the parent repo above the dependency dir is the parent dependency.
-    if [ ! -d "$START_TOR_AT_BOOT_PATH/../$parent_dep" ]; then
-      # Must load the dependency as any other fellow dependency if it is not
-      # a parent dependency.
-      load_required_dependency "$dependency_or_parent_path" "$required_dependency"
-    else
-      # Load the parent dependency.
-      # shellcheck disable=SC1090
-      source "$START_TOR_AT_BOOT_PATH/../$parent_dep/src/main.sh"
-    fi
+# Load dependencies that can be a parent dependency (=this module is a
+# dependency of that module/dependency).
+for parent_dep in "${START_TOR_AT_BOOT_PARENT_DEPS[@]}"; do
+  load_parent_dependency "calling_repo_root_path" "$START_TOR_AT_BOOT_PATH" "$parent_dep"
+done
 
-  done
-}
-
-load_dependencies
 LOG_LEVEL_ALL # set log level to all, otherwise, NOTICE, INFO, DEBUG, TRACE will not be logged.
 B_LOG --file log/multiple-outputs.txt --file-prefix-enable --file-suffix-enable
 
