1dd8a6d27284fb13193313b0cdc7a78685a35e4d
a-t-0 <you@example.com>
2023-12-05 22:51:51 +0100

    Prevend CLI args from being passed when loading dependency main.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,28 +1,30 @@
 #!/usr/bin/env bash
 
+SCRIPT_PATH=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+
 # Remove the submodules if they were still in the repo.
-git rm --cached test/libs/bats
-git rm --cached test/libs/bats-support
-git rm --cached test/libs/bats-assert
-git rm --cached dependencies/bash-package-installer
-git rm --cached dependencies/bash-log
+git rm --cached "$SCRIPT_PATH/test/libs/bats"
+git rm --cached "$SCRIPT_PATH/test/libs/bats-support"
+git rm --cached "$SCRIPT_PATH/test/libs/bats-assert"
+git rm --cached "$SCRIPT_PATH/dependencies/bash-package-installer"
+git rm --cached "$SCRIPT_PATH/dependencies/bash-log"
 
 # Remove and re-create the submodule directory.
-rm -r test/libs
-mkdir -p test/libs
+rm -r "$SCRIPT_PATH/test/libs"
+mkdir -p "$SCRIPT_PATH/test/libs"
 
 # Remove and create a directory for the dependencies.
 rm -r dependencies
 mkdir -p dependencies
 
 # (Re) add the BATS submodules to this repository.
-git submodule add --force https://github.com/sstephenson/bats test/libs/bats
-git submodule add --force https://github.com/ztombol/bats-support test/libs/bats-support
-git submodule add --force https://github.com/ztombol/bats-assert test/libs/bats-assert
-git submodule add --force https://github.com/hiveminds/bash-package-installer dependencies/bash-package-installer
-git submodule add --force https://github.com/hiveminds/bash-log dependencies/bash-log
+git submodule add --force https://github.com/sstephenson/bats "$SCRIPT_PATH/test/libs/bats"
+git submodule add --force https://github.com/ztombol/bats-support "$SCRIPT_PATH/test/libs/bats-support"
+git submodule add --force https://github.com/ztombol/bats-assert "$SCRIPT_PATH/test/libs/bats-assert"
+git submodule add --force https://github.com/hiveminds/bash-package-installer "$SCRIPT_PATH/dependencies/bash-package-installer"
+git submodule add --force https://github.com/hiveminds/bash-log "$SCRIPT_PATH/dependencies/bash-log"
 git submodule update --remote --recursive
 
 # Remove the submodules from the index.
-git rm -r -f --cached test/libs/bats
-git rm -r -f --cached dependencies
+git rm -r -f --cached "$SCRIPT_PATH/test/libs/bats"
+git rm -r -f --cached "$SCRIPT_PATH/dependencies"
