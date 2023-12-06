58b886aa4329730d8dd3f6f346e603b8cb3c1ad2
a-t-0 <you@example.com>
2023-12-06 01:46:07 +0100

    Prevend CLI args from being passed when loading dependency main.

--- src/installation.sh
+++ src/installation.sh
@@ -13,8 +13,9 @@ function install_tor_and_ssh_requirements() {
   ensure_apt_pkg "python3-virtualenv" 1
   ensure_apt_pkg "python3-pip" 1
   ensure_pip_pkg "dash" 0
-
   # ensure tor can be started without sudo.
-  sudo chmod 700 -R /var/lib/tor/*
-  sudo chown -R $whoami /var/lib/tor/
+  sudo chmod 700 -R $TOR_SERVICE_DIR/*
+  sudo chown -R $whoami $TOR_SERVICE_DIR
+  read -rp "ran: sudo chmod 700 -R $TOR_SERVICE_DIR/*"
+  read -rp "ran: sudo chown -R $whoami $TOR_SERVICE_DIR"
 }
