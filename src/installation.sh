c1b076aab0fdd6accd38edb7f9f71a835ba0a074
a-t-0 <you@example.com>
2024-04-09 12:54:38 +0200

    Removed python 3.10 venv requirement.

--- src/installation.sh
+++ src/installation.sh
@@ -9,7 +9,7 @@ function install_tor_and_ssh_requirements() {
   ensure_apt_pkg "torsocks" 0
   ensure_apt_pkg "tor" 1
   ensure_apt_pkg "jq" 1
-  ensure_apt_pkg "python3.10-venv" 1
+  # ensure_apt_pkg "python3.10-venv" 1
   ensure_apt_pkg "python3.11-venv" 1
   ensure_apt_pkg "python3-pip" 1
   ensure_apt_pkg "python3-virtualenv" 1
